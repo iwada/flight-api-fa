d5a35d92ba83186a97153e7523de6d1a7a010d6f
Iwada Eja <Iwada@twalle.com>
2022-05-11 23:20:38 +0300

    Basic JWT Authentication

---
+++ src/main/java/io/fa/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package io.fa.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import io.fa.models.RolesEnum;
+import io.fa.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(RolesEnum name);
+}
---
+++ src/test/java/io/fa/FaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
